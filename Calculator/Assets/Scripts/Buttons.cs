5e8952a6a4325d78928cf039bfedc9302555b26c
Just_PlayBoy <urovjohn@gmail.com>
2020-04-26 21:57:33 +0700

    Convert

--- Calculator/Assets/Scripts/Buttons.cs
+++ Calculator/Assets/Scripts/Buttons.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,43 +15,64 @@ public class Buttons : MonoBehaviour
 
     public void ClickButtonOne()
     {
-       text.text += "1";
+
+       text.text += "56";
+       int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonTwo()
     {
         text.text += "2";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonThree()
     {
         text.text += "3";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonFour()
     {
         text.text += "4";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonFive()
     {
         text.text += "5";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonSix()
     {
         text.text += "6";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonSeven()
     {
         text.text += "7";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonEight()
     {
         text.text += "8";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonNine()
     {
         text.text += "9";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     public void ClickButtonZero()
     {
         text.text += "0";
+        int number = Convert.ToInt32(text);
+        Debug.Log(number);
     }
     void Update()
     {
--- Calculator/Assets/Scripts/CalculatorLogic.cs
+++ Calculator/Assets/Scripts/CalculatorLogic.cs
@@ -1,10 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Globalization;
 using UnityEngine.UI;
 
 public class CalculatorLogic : MonoBehaviour
 {
+    public Text text;
+    private string[] strCount = new string[20];
+    private double[] intCount = new double[20];
+    private int number = 0;
+
+    public void Plus()
+    {
+        text.text += "+";
+    }
+    public void Equals()
+    {
+        text.text += "=";
+        for (int i = 0; i < number; i++)
+        {
+            intCount[i] = Convert.ToDouble(strCount[i]);
+            Console.WriteLine(intCount[i]);
+            Debug.Log("yes");
+        }
+    }
+    void CountNumbers()
+    {
+        for(int i = 0; i<20;i++)
+        {
+            strCount[i] = text.text;
+            number++;
+            if(text.text == "=")
+            {
+                break;
+            }
+        }
+    }
     void Start()
     {
         
