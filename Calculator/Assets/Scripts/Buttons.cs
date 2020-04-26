58dd33288825ebac7a72afb5e6e6a17aa124fd45
Just_PlayBoy <urovjohn@gmail.com>
2020-04-26 20:10:11 +0700

    Add FirstButton Script

---
+++ Calculator/Assets/Scripts/Buttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        
+    }
+
+    public void ClickButtons()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            text.text += "1";
+            Debug.Log("yes");
+        }
+    }
+    void Update()
+    {
+        ClickButtons();
+    }
+}
